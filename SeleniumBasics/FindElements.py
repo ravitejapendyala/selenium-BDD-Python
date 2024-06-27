62a3a426f6767e49a77db954105494804dd8b5bb
ravitejapendyala <raviteja.pendyalas@gmail.com>
2024-06-28 00:16:19 +0530

    Adding logic to get count of elements

--- SeleniumBasics/FindElements.py
+++ SeleniumBasics/FindElements.py
@@ -35,6 +35,6 @@ time.sleep(5)
 print(" Products count : ",len(Products))
 #.//table[@id='products-grid']//tbody/tr//td[3]
 for ele in Products:
-    print("Products list: ",ele.text)
+    print("Products list are : ",ele.text)
 driver.quit()
 
