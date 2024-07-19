aa1f6640eff188bae8386718ca11fbed0735a801
ravitejapendyala <raviteja.pendyalas@gmail.com>
2024-07-19 20:28:49 +0530

    Update AppCommands.py

--- SeleniumBasics/AppCommands.py
+++ SeleniumBasics/AppCommands.py
@@ -11,8 +11,9 @@ driver = webdriver.Chrome(service = service_obj)
 
 driver.get("https://opensource-demo.orangehrmlive.com/")
 print(driver.title)
-print(driver.page_source)
 print(driver.current_url)
+print(driver.page_source)
+
 driver.quit()
 
 
